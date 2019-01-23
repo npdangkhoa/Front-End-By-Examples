64a6e943b7130ca5ec544695ac9616ee276f9f39
Nguyen <knguyen97@vdc.csc.com>
2019-01-23 17:36:35 +0700

    move files

--- js-lib-reactjs/react-01-the-big-picture-02-working-with-data-step01-first-step/src/index.js
+++ js-lib-reactjs/react-01-the-big-picture-02-working-with-data-step01-first-step/src/index.js
@@ -3,57 +3,27 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 
-class Button extends React.Component{
-
-    handleClick =() =>{
-        this.props.onClickFunction(this.props.increaseVal);
-    };
-    render(){
-        return (
-            <button onClick={this.handleClick}>
-                    +{this.props.increaseVal}
-            </button>
-        );
-    }
+const Card = (props) => {
+    return(
+        <div>card</div>
+    );
 }
 
-const Result = (props) => {
-    console.log(props);
-    return (        
-        <div>Counter is: {props.text}</div>
+const ListCards = (props) => {
+    return(
+        <div>
+            <Card></Card>
+            <div>List Card</div>
+        </div>
     );
-};
+}
 
 class App extends React.Component{
 
-    state = {counter: 0};
-
-    handleClick = (increaseVal) => {
-        this.setState((preState)=> {
-            console.log(preState);
-            console.log("increaseVal: "+ increaseVal);
-
-            return {
-                counter: preState.counter + increaseVal
-            };
-        });
-       // console.log("counter : " +this.state.counter);
-    };
-
-    //TODO: 
-        //STEP01: move "state" valiable into global valible
-        //STEP02: caculate "state" valiable by <Button> tab.
-        //STEP03: pass "state.counter" valiable into <Result> tab.
-        //STEP04: get "state.counter" throw "props" paramter.
-        
     render(){
         return (
             <div>
-                 <Button increaseVal={1}   onClickFunction={this.handleClick}></Button>
-                 <Button increaseVal={5}   onClickFunction={this.handleClick}></Button>
-                 <Button increaseVal={10}  onClickFunction={this.handleClick}></Button>
-                 <Button increaseVal={100} onClickFunction={this.handleClick}></Button>
-                 <Result text={this.state.counter}></Result>
+                <ListCards></ListCards>
             </div>
         );
     }
